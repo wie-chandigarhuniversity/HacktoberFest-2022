38a84d1f4a1cb525cecf8be0b1e3066e21637f64
Aditya Singh <singhaditya6774@gmail.com>
2022-10-21 21:14:30 +0530

    Add files via upload

---
+++ Drumkit/index.js
@@ -0,0 +1,57 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+    
+  });
+}
+
+document.addEventListener ("keydown", function(){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+function makeSound(key){
+    switch (key) {
+        case "w":
+          var tom1 = new Audio("tom-1.mp3");
+          tom1.play();
+          break;
+        case "a":
+          var tom2 = new Audio("tom-2.mp3");
+          tom2.play();
+          break;
+        case "s":
+          var tom3 = new Audio("tom-3.mp3");
+          tom3.play();
+          break;
+        case "d":
+          var tom4 = new Audio("tom-4.mp3");
+          tom4.play();
+  
+        case "j":
+          var snare = new Audio("snare.mp3");
+          snare.play();
+          break;
+        case "k":
+          var crash = new Audio("crash.mp3");
+          crash.play();
+          break;
+        case "l":
+          var kick = new Audio("kick-bass.mp3");
+          kick.play();
+          break;
+        default:
+          console.log(buttonInnerHTML);
+      }
+
+}
+function buttonAnimation(currentKey){
+  var activeButton =document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },90);
+}
